3ea2ca89ed0cc84ceacbdb5e4120a90a9218e2b9
Nick Jay <b.nickjay05@gmail.com>
2021-05-15 19:22:40 +0800

    Revert "Merge branch 'master' of https://github.com/nikcoder2018/leadsopedia_admin"
    This reverts commit c59b771b9e9a34ecb6434c4479aebf31436db88a, reversing
    changes made to f24e51c2798923dc9be05e86942949b9ffb17fda.

--- config/database.php
+++ config/database.php
@@ -93,14 +93,11 @@ return [
         
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'leadsopedia_db'),
-            'username' => env('DB_USERNAME', 'admin'),
-            'password' => env('DB_PASSWORD', 'admin'),
+            'dsn' => 'mongodb+srv://leadsopedia_admin:sRtp3TtMrXEg@cluster0.qh12e.mongodb.net/leadsopedia_db?retryWrites=true&w=majority',
+            'database' => 'leadsopedia_db',
             'options' => [
-                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            ],
+                'database' =>  'admin' // sets the authentication database required by mongo 3
+            ]
         ],
     ],
 
